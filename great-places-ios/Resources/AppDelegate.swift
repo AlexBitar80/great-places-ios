0c6b8add2e37a350693ccc31dcb9023ec505b9f4
João Alexandre Bitar <joaoalexandre.bitar@gmail.com>
2024-05-18 00:58:46 -0300

    Adiciona estrutura inicial do projeto

--- great-places-ios/Resources/AppDelegate.swift
+++ great-places-ios/Resources/AppDelegate.swift
@@ -7,14 +7,15 @@
 
 import UIKit
 import CoreData
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        GMSServices.provideAPIKey(Environments.GoogleMapsAPIKey)
+        
         return true
     }
 
